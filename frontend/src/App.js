ff969969378c9f637bf73b1275af89a64bf63211
SantiFagliano <santifagliano@live.com.ar>
2021-01-04 23:01:56 -0300

    creamos el enrutador mas conocido como router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import Navigation from './components/Navigation';
+import NoteList from './components/NotesList';
+import CreateNote from './components/CreateNote';
+import CreateUser from './components/CreateUser';
 
 function App() {
   return (
-    <div>Hello word</div>
+    <Router>
+      <Navigation />
+      <Route exact path="/" component={NoteList} />
+      <Route exact path="/edit/:id" component={CreateNote} />
+      <Route exact path="/create" component={CreateNote} />
+      <Route exact path="/user" component={CreateUser} />
+    </Router>
   );
 }
 
